bdb044fa73d86abf38f086b69eb01df6dfa14233
Danielle <42225162+stereoke@users.noreply.github.com>
2018-12-04 21:00:37 -0700

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,26 @@
+//set count
+if(localStorage.getItem("count") == null){
+	localStorage.setItem("count", 0)
+}
+
+//update count
+function update(){
+	document.getElementById("count").innerHTML = localStorage.getItem("count");
+
+}
+
+update()
+//add one to count
+function plusOne() {
+	localStorage.setItem("count", (Number(localStorage.getItem("count")) + 1));
+	update();
+}
+
+//reset count
+function reset() {
+	if (confirm("Really, no HUGS???")) {
+    	localStorage.setItem("count", 0);
+		update();
+	}
+}
+
